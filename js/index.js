046bb821daf3cf96210fa69efadc1c4e8c73939a
unknown <pankajkardam007.com>
2025-07-05 17:31:47 +0530

    add pages

--- js/index.js
+++ js/index.js
@@ -1,14 +1,51 @@
-$(document).ready(function () {
-  $(".at-title").click(function () {
-    $(this)
-      .toggleClass("active")
-      .next(".at-tab")
-      .slideToggle()
-      .parent()
-      .siblings()
-      .find(".at-tab")
-      .slideUp()
-      .prev()
-      .removeClass("active");
-  });
-});
+// $(document).ready(function () {
+//   $(".at-title").click(function () {
+//     $(this)
+//       .toggleClass("active")
+//       .next(".at-tab")
+//       .slideToggle()
+//       .parent()
+//       .siblings()
+//       .find(".at-tab")
+//       .slideUp()
+//       .prev()
+//       .removeClass("active");
+//   });
+// });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// $(document).ready(function () {
+// $('.connect_wallet').click(function() {
+//         // $('.connect_wallet').css({
+          
+//         //  'background-image': 'linear-gradient(to right, #007991 0%, #78ffd6  51%, #007991  100%)',
+//         //     // 'margin': '10px',
+//         //     // 'padding': '15px 45px',
+//         //     // 'text-align': 'center',
+//         //     'text-transform': 'uppercase',
+//         //     'transition': '0.5s',
+//         //     'background-size': '200% auto',
+//         //     // color: white;            
+//         //     // box-shadow: 0 0 20px #eee;
+//         //     // border-radius: 10px;
+//         //     // display: block;
+          
+
+//         //   // .btn-grad:hover {
+//         //   //   background-position: right center; /* change the direction of the change here */
+//         //   //   color: #fff;
+//         //   //   text-decoration: none;
+//         //   // }
+         
+//         // })
+//         $('.connect_wallet').html('Xa00..cbtse')
+       
+//     });
+//     });
